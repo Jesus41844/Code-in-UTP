03ccb33dad82e5b58269a431dcf3b77b2d0f647f
Jesus41844 <degraciajesus831@gmail.com>
2024-11-19 16:23:12 -0500

    Proyecto final 95% listo

--- ProyectoFinal/ProyectoFinalHPA.cpp
+++ ProyectoFinal/ProyectoFinalHPA.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 #include<fstream>
+#include<sstream>
 #include<string>
 using namespace std;
+
 ifstream readfile;
+ofstream editFile;
 
 struct Clientes {
     string nombre;
-    int pedido;
+    string pedido;
+    string pizza;
+    string bebidas;
 };
 
 
@@ -24,27 +29,47 @@ void bienvenida(){
     readfile.close();
 }
 
+void pedidos(const Clientes& cliente){
+    editFile.open("C:/Users/j3sus/OneDrive/Documents/Programacion/Code/ProyectoFinal/pedidos.txt", ios::app);
+    if (editFile.is_open()) {
+        editFile << cliente.nombre << endl << cliente.pedido << endl << cliente.pizza << endl << cliente.bebidas << endl;
+        editFile.close();
+        string word;
+    } else {
+        cout << "Error al editar el archivo";
+    }
+}
+
 int main() {
+    Clientes Cliente;
     char PizzBeb, seguir;
-    string sabores;
+    int contador = 0;
+    stringstream sc;
     bool continuar = true;
-    cout << "Bienvenido a McDeallo";
+    cout << "Bienvenido a McDeallo" << endl << "Cual es su nombre: ";
+    cin>>Cliente.nombre;
     while (continuar) {
         bienvenida();
         cin>>PizzBeb;
         if (PizzBeb == 'p') {
             cout << "Decide los sabores de la pizza: ";            
             for (int i = 0; i <= 4; i++) {
-                cin>>sabores;
+                cin>>Cliente.pizza;
             }
         } else if (PizzBeb == 'b') {
             cout << "Elige tu bebida: ";
+            cin>>Cliente.bebidas;
         }
-        cout << "Desea ordenar algo mas? ";
+        cout << "Desea ordenar algo mas?, n/N para terminar el pedido: "<<endl;
         cin >> seguir;
-        if (!seguir == 's' or !seguir == 'S'){
+        if (seguir == 'N' || seguir == 'n'){
             continuar = false;
         }
+        sc << contador;
+        string nCliente = sc.str();
+        Cliente.pedido = nCliente;
+        pedidos(Cliente);
+        contador++;
     }
     return 0;
 }
